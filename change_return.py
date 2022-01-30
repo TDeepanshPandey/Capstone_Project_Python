780d0fc6e6c99df2821b2724546a58b9a2ff0b6f
Deepansh Pandey <easypandey.dp@gmail.com>
2022-01-31 00:43:44 +0100

    Added change return program new problems

--- change_return.py
+++ change_return.py
@@ -1,3 +1,14 @@
+import math
 if __name__ == '__main__':
-    cost = int(input("Enter the cost :"))
-    money = int(input("Money paid :"))
+    cost = float(input("Enter the cost : "))
+    money = float(input("Money paid : "))
+    change = round(money - cost, 3)
+    print("Return Change : ", change)
+    change = change * 100
+    quarter = math.floor(change / 25)
+    change = change % 25
+    dime = math.floor(change / 10)
+    change = change % 10
+    nickel = math.floor(change / 5)
+    penny = int(change % 5)
+    print("Return : \n Quarter : {} \n Dime : {} \n Nickel : {} \n Penny : {} ".format(quarter,dime,nickel, penny))
