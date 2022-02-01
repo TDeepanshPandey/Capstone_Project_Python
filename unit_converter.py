0ebcf4f6bf871e4e7451198d324b2c976190ef48
Deepansh Pandey <easypandey.dp@gmail.com>
2022-02-01 23:53:26 +0100

    Created a simple unit converter program with OOP

--- unit_converter.py
+++ unit_converter.py
@@ -1,3 +1,16 @@
-if __name__ == "__main__":
-    unit = input("Enter your choice for conversion: \n 1. Temp \n 2. Currency \n 3. Volume \n 4. Mass \n")
+def mass_convert(w: int):
+    return w*100
 
+
+def temp_convert(t: int):
+    pass
+
+
+if __name__ == "__main__":
+    unit = int(input("Enter your choice for conversion: \n 1. Temp \n 2. Mass \n"))
+    if unit == 2:
+        weight = int(input("Enter the weight in kgs : "))
+        print("Mass in grams", mass_convert(weight))
+    if unit == 1:
+        temp = int(input("Enter temperature in Celsius : "))
+        print("Temperature in Fahrenheit : ", temp_convert(temp))
